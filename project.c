04dfa2bbf54baae83eacf20aaae13838931966c7
insertnamehere691 <rgrau0827@gmail.com>
2024-11-19 16:40:37 -0500

    Update project.c
    Group Project CDA

--- project.c
+++ project.c
@@ -5,7 +5,24 @@
 /* 10 Points */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
-
+switch(ALUControl){
+    case 0: *ALUresult = A + B;
+    break;
+    case 1: *ALUresult = A - B;
+    break;
+    case 2: *ALUresult = (int)A < (int)B; 
+    break;
+    case 3: *ALUresult = A < B;
+    break;
+    case 4: *ALUresult = A && B;
+    break;
+    case 5: *ALUresult = A || B;
+    break;
+    case 6: *ALUresult = B << 16;
+    break;
+    case 7: *ALUresult = -A;
+    break;
+    
 }
 
 int instruction_fetch(unsigned PC, unsigned *Mem, unsigned *instruction)
